88645a4387bdc3dd8070e38b47c024a2844ff8f8
Liz Meeker <lizmeeker90@gmail.com>
2022-06-27 20:49:41 -0400

    added painting handler

--- src/App.js
+++ src/App.js
@@ -13,28 +13,46 @@ class App extends Component {
         className='grid-cell'
         key={`${x}, ${y}`}
         style={{backgroundColor: '#' + cell.color}}
+        onClick={() => this.paintHandler(x, y)}
       >
       </div>
     )
-  }
+  };
 
   renderGrid = () => {
     let result = this.state.board.map((row, y) => {
       return row.map((cell, x) => {
         return this.renderGridCell(cell, x, y)
       })
-    })
+    });
     return (
       <div className='grid-container'>
         { result }
       </div>
     )
-  }
-  
+  };
+
+  paintHandler = (x, y) => {
+    if (this.state.board[y][x].life === false) {
+      this.state.board[y][x] = {
+        life: true,
+        color: "000000"
+      }
+    }
+    else if (this.state.board[y][x].life === true) {
+      this.state.board[y][x] = {
+        life: false,
+        color: "FFFFFF"
+      }
+    };
+    this.forceUpdate();
+  };
+
   render() { 
     return (
       <div className='page'>
         {this.renderGrid()}
+        <button>Next Step</button>
       </div>
     );
   }
