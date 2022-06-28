ecb5941bca935cf8fd550f7b6e271dfebfa3faa3
Liz Meeker <lizmeeker90@gmail.com>
2022-06-27 21:34:49 -0400

    fixed canvas edge validation

--- src/Utilities/EngineUtils/EngineUtils.js
+++ src/Utilities/EngineUtils/EngineUtils.js
@@ -10,22 +10,22 @@ const generateNextBoard = board => {
       if (y > 0 && board[y - 1][x].life === true) {
         count += 1
       };
-      if (y > 0 && x <= board[y].length && board[y - 1][x + 1].life === true) {
+      if (y > 0 && x + 1 < board[y].length && board[y - 1][x + 1].life === true) {
         count += 1
       };
       if (x > 0 && board[y][x - 1].life === true) {
         count += 1
       };
-      if (x <= board[y].length && board[y][x + 1].life === true) {
+      if (x + 1 < board[y].length && board[y][x + 1].life === true) {
         count += 1
       };
-      if (y <= board.length && x > 0 && board[y + 1][x - 1].life === true) {
+      if (y + 1 < board.length && x > 0 && board[y + 1][x - 1].life === true) {
         count += 1
       };
-      if (y <= board.length && board[y + 1][x].life === true) {
+      if (y + 1 < board.length && board[y + 1][x].life === true) {
         count += 1
       };
-      if (y <= board.length && x <= board[y].length && board[y + 1][x + 1].life === true) {
+      if (y + 1 < board.length && x + 1 < board[y].length && board[y + 1][x + 1].life === true) {
         count += 1
       };
       if (count < 2 || count > 3) {
