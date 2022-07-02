5e9db55ddd9dc8aac32478c4e419182219685a52
Liz Meeker <lizmeeker90@gmail.com>
2022-07-02 13:29:25 -0400

    added additional colors to palette

--- src/Utilities/Colors/colors.js
+++ src/Utilities/Colors/colors.js
@@ -11,34 +11,194 @@ const colors = [
   },
 
   {
+    name: "dark grey",
+    value: {
+      red: 127,
+      green: 127,
+      blue: 127
+    },
+    id: 5
+  },
+
+  {
+    name: "dark red",
+    value: {
+      red: 136,
+      green: 0,
+      blue: 21
+    },
+    id: 6
+  },
+
+  {
     name: "red",
     value: {
-      red: 255,
-      green: 0,
-      blue: 0
+      red: 237,
+      green: 28,
+      blue: 36
     },
     id: 2
   },
 
   {
+    name: "orange",
+    value: {
+      red: 255,
+      green: 127,
+      blue: 39
+    },
+    id: 7
+  },
+
+  {
+    name: "yellow",
+    value: {
+      red: 255,
+      green: 242,
+      blue: 0
+    },
+    id: 8
+  },
+
+  {
     name: "green",
     value: {
-      red: 0,
-      green: 255,
-      blue: 0
+      red: 34,
+      green: 177,
+      blue: 76
     },
     id: 3
   },
 
   {
-    name: "blue",
+    name: "turquoise",
     value: {
       red: 0,
-      green: 0,
-      blue: 255
+      green: 162,
+      blue: 232
     },
     id: 4
-  }
+  },
+
+  {
+    name: "indigo",
+    value: {
+      red: 63,
+      green: 72,
+      blue: 204
+    },
+    id: 9
+  },
+
+  {
+    name: "purple",
+    value: {
+      red: 63,
+      green: 72,
+      blue: 204
+    },
+    id: 10
+  },
+
+  {
+    name: "white",
+    value: {
+      red: 255,
+      green: 255,
+      blue: 255
+    },
+    id: 11
+  },
+
+  {
+    name: "light grey",
+    value: {
+      red: 195,
+      green: 195,
+      blue: 195
+    },
+    id: 12
+  },
+
+  {
+    name: "brown",
+    value: {
+      red: 185,
+      green: 122,
+      blue: 87
+    },
+    id: 13
+  },
+
+  {
+    name: "rose",
+    value: {
+      red: 255,
+      green: 174,
+      blue: 201
+    },
+    id: 14
+  },
+
+  {
+    name: "gold",
+    value: {
+      red: 255,
+      green: 201,
+      blue: 14
+    },
+    id: 15
+  },
+
+  {
+    name: "light yellow",
+    value: {
+      red: 239,
+      green: 228,
+      blue: 176
+    },
+    id: 16
+  },
+
+  {
+    name: "lime",
+    value: {
+      red: 181,
+      green: 220,
+      blue: 29
+    },
+    id: 17
+  },
+
+  {
+    name: "light blue",
+    value: {
+      red: 153,
+      green: 217,
+      blue: 234
+    },
+    id: 18
+  },
+
+  {
+    name: "blue-gray",
+    value: {
+      red: 112,
+      green: 146,
+      blue: 190
+    },
+    id: 19
+  },
+
+  {
+    name: "lavender",
+    value: {
+      red: 200,
+      green: 191,
+      blue: 231
+    },
+    id: 20
+  },
 
 ];
 
