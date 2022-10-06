305a931ccea468431db128e5437d55659e757cc3
Liz Meeker <lizmeeker90@gmail.com>
2022-10-06 13:29:54 -0400

    menu bar repositioned and functions refactored

--- src/pages/CanvasPage/CanvasPage.js
+++ src/pages/CanvasPage/CanvasPage.js
@@ -134,6 +134,7 @@ class CanvasPage extends Component {
       <div
         className='canvas'
         style={{width: gridWidth, height: '1200px'}}
+        onClick={() => this.expandMenuHandler()}
       >
         { grid }
       </div>
@@ -286,7 +287,7 @@ class CanvasPage extends Component {
         <PageHeader />
         <main className='page'>
           <div className='menu'>
-            {/* <select className='menu__preset-select' onChange={this.retrieveSelectedPreset}>
+            <select className='menu__preset-select' onChange={this.retrieveSelectedPreset}>
               <option className='menu__preset-option' value="">- Load a Premade Canvas -</option>
               {
                 presetList.map(canvas => (
@@ -299,116 +300,100 @@ class CanvasPage extends Component {
                   </option>
                 ))
               }
-            </select> */}
-            <div className='options'>
-              <button className='options__title' onClick={() => this.expandMenuHandler('options')}> Options </button>
-              <div className={this.state.visibleMenu === 'options' ? 'options__visible' : 'options__hidden'}>
-                <div className='options__button-container'>
-                  <button className="options__button" onClick={this.playHandler}>
-                    <img className="options__button-image" src={this.state.playing ? pauseIcon : playIcon} alt="play/pause"/>
-                  </button>
-                  <button className="options__button" onClick={this.stepHandler}>
-                    <img className="options__button-image" src={nextIcon} alt="next step"/>
-                  </button>
-                  <button className="options__button" onClick={this.randomBoardHandler}>
-                    <img className="options__button-image" src={randomIcon} alt="random canvas"/>
-                  </button>
-                  <button className="options__button" onClick={this.clearCanvasHandler}>
-                    <img className="options__button-image" src={clearIcon} alt="clear canvas"/>
-                  </button>
-                  <button className="options__button" onClick={this.saveHandler}>
-                    <img className="options__button-image" src={saveIcon} alt="save"/>
-                  </button>
-                  <button className="options__button" onClick={this.loadHandler}>
-                    <img className="options__button-image" src={loadIcon} alt="load"/>
-                  </button>
-                </div>
+            </select>
+            <div className='controls'>
+              <button className='controls__title' onClick={() => this.expandMenuHandler('controls')}> Controls </button>
+              <div className={this.state.visibleMenu === 'controls' ? 'controls__visible' : 'controls__hidden'}>
+                <button className="controls__button" onClick={this.playHandler}>
+                  <img className="controls__button-image" src={this.state.playing ? pauseIcon : playIcon} alt="play/pause"/>
+                </button>
+                <button className="controls__button" onClick={this.stepHandler}>
+                  <img className="controls__button-image" src={nextIcon} alt="next step"/>
+                </button>
+                <button className="controls__button" onClick={this.randomBoardHandler}>
+                  <img className="controls__button-image" src={randomIcon} alt="random canvas"/>
+                </button>
+                <button className="controls__button" onClick={this.clearCanvasHandler}>
+                  <img className="controls__button-image" src={clearIcon} alt="clear canvas"/>
+                </button>
+                <button className="controls__button" onClick={this.saveHandler}>
+                  <img className="controls__button-image" src={saveIcon} alt="save"/>
+                </button>
+                <button className="controls__button" onClick={this.loadHandler}>
+                  <img className="controls__button-image" src={loadIcon} alt="load"/>
+                </button>
               </div>
             </div>
             <div className='palette'>
               <button className='palette__title' onClick={() => this.expandMenuHandler('palette')}> Palette </button>
               <div className={this.state.visibleMenu === 'palette' ? 'palette__visible' : 'palette__hidden'}>
-                <div className='palette__container'>
-                  {
-                    colors.map(color => {
-                      return (
-                        <button
-                          className='palette__button'
-                          key={color.id}
-                          onClick={() => this.colorSelectHandler(color.name)}
-                          style={this.state.colorName === color.name ? {boxShadow: "inset 0px 0px 10px 4px #999"} : {boxShadow: "none"}}
+                {
+                  colors.map(color => {
+                    return (
+                      <button
+                        className='palette__button'
+                        key={color.id}
+                        onClick={() => this.colorSelectHandler(color.name)}
+                        style={this.state.colorName === color.name ? {boxShadow: "inset 0px 0px 10px 4px #999"} : {boxShadow: "none"}}
+                      >
+                        <div
+                          className='palette__button--color'
+                          style={{backgroundColor: `rgb(${color.value.red}, ${color.value.green}, ${color.value.blue})`}}
                         >
-                          <div
-                            className='palette__button--color'
-                            style={{backgroundColor: `rgb(${color.value.red}, ${color.value.green}, ${color.value.blue})`}}
-                          >
-                          </div>
-                        </button>
-                      )
-                    })
-                  }
-                </div>
+                        </div>
+                      </button>
+                    )
+                  })
+                }
               </div>
             </div>
             <div className='tools'>
               <button className='tools__title' onClick={() => this.expandMenuHandler('tools')}> Tools </button>
               <div className={this.state.visibleMenu === 'tools' ? 'tools__visible' : 'tools__hidden'}>
-                <div className='tools__container'>
-                  <button
-                    className='tools__item'
-                    onClick={() => this.brushSelectHandler("Dot")}
-                    style={this.state.brushName === "Dot" ? {boxShadow: "inset 0px 0px 10px 4px #999"} : {boxShadow: "none"}}
-                  >
-                    <img className='tools__item-image' src={pencilIcon} alt="pencil"/>
-                  </button>
-                  <button
-                    className='tools__item'
-                    onClick={this.eraseHandler}
-                    style={this.state.erase ? {boxShadow: "inset 0px 0px 10px 4px #999"} : {boxShadow: "none"}}
-                  >
-                    <img src={eraserIcon} alt="eraser"/>
-                  </button>
-                </div>
+                <button
+                  className='tools__item'
+                  onClick={() => this.brushSelectHandler("Dot")}
+                  style={this.state.brushName === "Dot" ? {boxShadow: "inset 0px 0px 10px 4px #999"} : {boxShadow: "none"}}
+                >
+                  <img className='tools__item-image' src={pencilIcon} alt="pencil"/>
+                </button>
+                <button
+                  className='tools__item'
+                  onClick={this.eraseHandler}
+                  style={this.state.erase ? {boxShadow: "inset 0px 0px 10px 4px #999"} : {boxShadow: "none"}}
+                >
+                  <img src={eraserIcon} alt="eraser"/>
+                </button>
               </div>
             </div>
             <div className='brushes'>
               <button className='brushes__title' onClick={() => this.expandMenuHandler('patterns')}> Patterns </button>
               <div className={this.state.visibleMenu === 'patterns' ? 'brushes__visible' : 'brushes__hidden'}>
-                <div className='brushes__list'>
-                  {this.renderToolItems("pattern")}
-                </div>
+                {this.renderToolItems("pattern")}
               </div>
             </div>
             <div className='brushes'>
               <button className='brushes__title' onClick={() => this.expandMenuHandler('stillLifes')}> Still Lifes </button>
               <div className={this.state.visibleMenu === 'stillLifes' ? 'brushes__visible' : 'brushes__hidden'}>
-                <div className='brushes__list'>
-                  {this.renderToolItems("still life")}
-                </div>
+                {this.renderToolItems("still life")}
               </div>
             </div>
             <div className='brushes'>
               <button className='brushes__title' onClick={() => this.expandMenuHandler('oscillators')}> Oscillators </button>
               <div className={this.state.visibleMenu === 'oscillators' ? 'brushes__visible' : 'brushes__hidden'}>
-                <div className='brushes__list'>
-                  {this.renderToolItems("oscillator")}
-                </div>
+                {this.renderToolItems("oscillator")}
               </div>
             </div>
             <div className='brushes'>
               <button className='brushes__title' onClick={() => this.expandMenuHandler('spaceships')}> Spaceships </button>
               <div className={this.state.visibleMenu === 'spaceships' ? 'brushes__visible' : 'brushes__hidden'}>
-                <div className='brushes__list'>
-                  {this.renderToolItems("spaceship")}
-                </div>
+                {this.renderToolItems("spaceship")}
               </div>
             </div>
             <div className='brushes'>
               <button className='brushes__title' onClick={() => this.expandMenuHandler('guns')}> Guns </button>
               <div className={this.state.visibleMenu === 'guns' ? 'brushes__visible' : 'brushes__hidden'}>
-                <div className='brushes__list'>
-                  {this.renderToolItems("gun")}
-                </div>
+                {this.renderToolItems("gun")}
               </div>
             </div>
           </div>
